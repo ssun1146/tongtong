bbd54d283c547cbb3e8a5d397813da09cdd40fe8
ssunhwa <ssun134568@gmail.com>
2021-09-27 11:12:00 +0900

    자바스크립트 강의 part2 for문관련 끄적

--- js/tab.js
+++ js/tab.js
@@ -12,6 +12,8 @@ $(".tab-button").click(function(){
 var tabLength = $(".tab-button").length();
 
 // for 반복문에서 var 사용하지 말고 let을 사용해야함. for 반복문 안에 i 변수가 있을 때 let으로 바꿔야 잘 동작.
+// var 사용하면, for 반복문 이상으로 범위가 넓어지는데,
+// let 사용하면, for 반복문 내에서 참조할 수 있는 범위를 가짐!
 for ( let  i = 0; i < tabLength ; i++ ){ 
     $(".tab-button").eq(i).click(function(){
         $(".tab-button").removeClass("active");
