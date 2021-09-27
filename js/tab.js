86849fb19dac1cb7c450ea513561a9ad96055d96
ssunhwa <ssun134568@gmail.com>
2021-09-27 11:04:31 +0900

    자바스크립트 Part2 탭 만들기 (for문사용)

---
+++ js/tab.js
@@ -0,0 +1,24 @@
+$(".tab-button").click(function(){
+    var num = $(".tab-button").index(this); // index($(this)) 인지 index(this)인지 맨날 헷갈린다 ~.~
+
+    $(".tab-button").removeClass("active");
+    $(".tab-content").removeClass("show");
+
+    $(this).addClass("active");
+    $(".tab-content").eq(num).addClass("show");
+})
+
+// 위에 작성한 코드 for 문으로 쓰기..!!!!
+var tabLength = $(".tab-button").length();
+
+// for 반복문에서 var 사용하지 말고 let을 사용해야함. for 반복문 안에 i 변수가 있을 때 let으로 바꿔야 잘 동작.
+for ( let  i = 0; i < tabLength ; i++ ){ 
+    $(".tab-button").eq(i).click(function(){
+        $(".tab-button").removeClass("active");
+        $(".tab-content").removeClass("show");
+
+        $(this).addClass("active");
+        $(".tab-content").eq(i).addClass("show");
+    })
+}
+
